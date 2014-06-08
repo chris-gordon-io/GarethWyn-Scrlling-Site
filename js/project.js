3802be7b280c0bbecef8e9577f27e9857d6729f3
Eightarmedpet <hi@eightarmedpet.com>
2014-06-08 12:25:49 +0100

    JS update
    FUCKING GITHUB

--- js/project.js
+++ js/project.js
@@ -46,13 +46,14 @@
     $("nav a[href*=#]");
     //Slideshow
 
-$(function(){
-    $(".fadein img:gt(0)").hide();
-    setInterval(function () {
-      $(".fadein :first-child").fadeOut()
+    $(function () {
+        $(".fadein img:gt(0)").hide();
+        setInterval(function () {
+            $(".fadein :first-child").fadeOut()
          .next("img").fadeIn()
-         .end().appendTo(".fadein");}, 
-      3000);
-});
+         .end().appendTo(".fadein");
+        },
+        3000);
+    });
 
 } (jQuery));
